544528246e17f6ab133ad3ea6de6ccb0ba714752
Marius.Politze <Marius.Politze@67dcfa50-b739-11dd-8622-756dacf128ff>
2008-11-21 00:40:37 +0000

    - First JXMapviever Test - Added German postal code resources

--- trunk/JGeoViz/src/de/rwth/matse/jgeoviz/HelloWorld.java
+++ trunk/JGeoViz/src/de/rwth/matse/jgeoviz/HelloWorld.java
@@ -1,7 +1,18 @@
 package de.rwth.matse.jgeoviz;
 
+import javax.swing.*;
+import org.jdesktop.swingx.*;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		JXMapKit k = new JXMapKit();
+		JFrame f = new JFrame();
+		
+		k.setDefaultProvider(org.jdesktop.swingx.JXMapKit.DefaultProviders.OpenStreetMaps);
+		
+		f.add(k);
+		
+		f.setVisible(true);
+		
 	}
 }
